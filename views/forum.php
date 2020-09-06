a7a208a62e7948198ccd4c29de6906b705c04544
bréval <breval2000@live.fr>
2020-09-07 00:33:45 +0200

    forum listing need

--- views/forum.php
+++ views/forum.php
@@ -4,4 +4,21 @@ include_once "includes/composants/nav-bar.php";
 
 ?>
 
-<h2>Le super Forum</h2>
+<h2>Forum</h2>
+
+<h3>Discute avec tous le monde ! Et apprends de nouvelles choses !</h3>
+
+<a href="#">Créer un post</a>
+
+<div class="list-posts">
+    <h2>Sujet ouverts en ce moment </h2>
+    <div class="posts">
+        <h3>Titre</h3>
+        <p>description</p>
+        <p>autheur</p>
+        <p>mathiere</p>
+        <p>Ouverte / fermer</p>
+        <p>Nombre de vote</p>
+        <p>Nombre de com</p>
+    </div>
+</div>
