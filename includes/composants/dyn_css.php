35275e49f93cbea82ceeec715bba5e4f2756a246
lightin <breval2000@live.fr>
2020-09-07 14:12:24 +0200

    forum list dyn

---
+++ includes/composants/dyn_css.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="../../ressources/css/style.css">
+
+<?php
+
+
+
+?>
+
+</head>
+<body>
+<div class="page">
+
+
+    // On verra plus tard, flemme pour le moment
--- views/forum.php
+++ views/forum.php
@@ -2,23 +2,89 @@
 
 include_once "includes/composants/nav-bar.php";
 
+
+// POST
+
+//$url = 'http://server.com/path';
+//$data = array('key1' => 'value1', 'key2' => 'value2');
+//
+//// use key 'http' even if you send the request to https://...
+//$options = array(
+//    'http' => array(
+//        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+//        'method' => 'POST',
+//        'content' => http_build_query($data)
+//    )
+//);
+//$context = stream_context_create($options);
+//$result = file_get_contents($url, false, $context);
+//if ($result === FALSE) { /* Handle error */
+//}
+
+
+// GET
+$posts = json_decode(file_get_contents("http://localhost:4567/api/getForumQuestions"));
+
+
+//var_dump($posts);
+
 ?>
+
 
 <h2>Forum</h2>
 
 <h3>Discute avec tous le monde ! Et apprends de nouvelles choses !</h3>
 
 <a href="#">Créer un post</a>
-
 <div class="list-posts">
     <h2>Sujet ouverts en ce moment </h2>
-    <div class="posts">
-        <h3>Titre</h3>
-        <p>description</p>
-        <p>autheur</p>
-        <p>mathiere</p>
-        <p>Ouverte / fermer</p>
-        <p>Nombre de vote</p>
-        <p>Nombre de com</p>
-    </div>
+
+    <?php
+    foreach ($posts as $p) {
+        ?>
+
+
+        <div class="posts">
+            <h3>
+                <?php
+                echo $p->titre;
+                ?>
+            </h3>
+            <p>
+                <?php
+                echo $p->description;
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $p->prenom;
+                ?>
+            </p>
+            <p>
+                <?php
+                echo $p->intitule;
+                ?>
+            </p>
+            <p>
+                status de la question :
+                <?php
+                echo $p->status;
+                ?>
+            </p>
+            <p>Nombre de vote :
+                <?php
+                echo $p->votes;
+                ?>
+
+            </p>
+            <p>Nombre de com :
+                <?php
+                echo $p->comments;
+                ?>
+            </p>
+        </div>
+        <?php
+    }
+    ?>
+
 </div>
