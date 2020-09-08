8f6d7ed2dbcf32b5f2bcc8ad21e7ddeaef980511
lightin <breval2000@live.fr>
2020-09-08 16:49:05 +0200

    Mise en place du site statique

--- includes/composants/nav-bar.php
+++ includes/composants/nav-bar.php
@@ -1,6 +1,6 @@
 <header class="navHeader">
     <div id="navTop">
-        <a href="#"><img src="/ressources/img/favicon.png" alt="logo"></a>
+        <a href="#"><img src="/ressources/img/favicon.png" alt="logo" class="logo"></a>
         <a class="disabled" onclick="clickNavBtn();" id="navBtnPhone">
             <div id="navBtn"></div>
         </a>
@@ -12,19 +12,19 @@
                 <li><a href="#" onclick="clickNavBtn();">Les cours</a></li>
                 <li><a href="#" onclick="clickNavBtn();">Donner un cours</a></li>
                 <li><a href="#" onclick="clickNavBtn();">Suggérer un cours</a></li>
-                <li><a href="#" onclick="clickNavBtn();">Forum</a></li>
+                <li><a href="/forum" onclick="clickNavBtn();">Forum</a></li>
                 <li><a href="#" onclick="clickNavBtn();">Mon profil</a></li>
             </ul>
         </nav>
     </div>
     <div id="navDesktop">
         <ul>
-            <li><a href="#"><img src="images" alt="logo"></a></li>
+            <li><a href="#"><img src="/ressources/img/favicon.png" alt="logo" class="logo"></a></li>
             <li><a href="#">Scratch Overflow</a></li>
             <li><a href="#">Les cours</a></li>
             <li><a href="#">Donner un cours</a></li>
             <li><a href="#">Suggérer un cours</a></li>
-            <li><a href="#">Forum</a></li>
+            <li><a href="/forum">Forum</a></li>
             <!--            todo si connecter afficher 'Mon profil', si ce n'est pas le cas, afficher 'Connexion / Inscription' -->
             <li><a href="#">Mon profil</a></li>
         </ul>
