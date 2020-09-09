fcd6acfc3e6965b5638e89d4a27c1f11e79d2750
lightin <breval2000@live.fr>
2020-09-09 10:28:37 +0200

    Change JS in router

--- includes/router.php
+++ includes/router.php
@@ -9,11 +9,12 @@ switch ($request) {
     case '/' :
         header_options(["style", "nav", "button", "card"]);
         require 'views/home.php';
-        footer_options(["lottie","navBtn",]);
+        footer_options(["lottie","navBtn"]);
         break;
     case '/cours' :
         header_options(["style", "nav", "button","card"]);
         require 'views/cours.php';
+        footer_options(["lottie","navBtn","fonction"]);
         break;
     case '/about' :
         header_options(["style", "nav", "button"]);
@@ -22,14 +23,17 @@ switch ($request) {
     case '/forum' :
         header_options(["style", "nav", "button", "card", "cardForum"]);
         require 'views/forum.php';
+        footer_options(["lottie","navBtn","fonction"]);
         break;
     case '/admin' :
         header_options(["style", "nav", "button", "card"]);
         require 'views/panel_admin.php';
+        footer_options(["lottie","navBtn","fonction"]);
         break;
     case '/forum/create' :
         header_options(["style", "forum", "nav", "button"]);
         require 'views/forum_create.php';
+        footer_options(["lottie","navBtn","fonction"]);
         break;
     case (preg_match('/\/forum\/./', $request) ? true : false) :
         header_options(["style", "forum", "nav", "button"]);
