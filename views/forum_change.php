fbff9bc4d43718b89e9d2c32da3c707e4895759d
bréval <breval2000@live.fr>
2020-10-06 23:04:28 +0200

    Preremplir form

--- views/forum_change.php
+++ views/forum_change.php
@@ -10,32 +10,33 @@ $question = hget("http://localhost:4567/api/getQuestion/" . $id_question)[0];
 ?>
 
 
-<div class="login-box">
-    <h2>Créer un nouveau sujet !</h2>
-    <form class="in_box" method="post">
-        <div class="user-box">
-            <input type="text" class="title" name="title" required value="<?php echo $question->titre?>" >
-            <label>Titre :</label>
-        </div>
-        <div class="user-box">
-            <select name="id_matiere" id="matiere-select" required>
-                <option value="">Matière</option>
-                <?php
-                foreach (hget("http://localhost:4567/api/getMatiere") as $m) {
-                    echo "<option value=" . $m->id_matiere . ">" . $m->intitule . "</option>";
-                }
-                ?>
-            </select>
-        </div>
-        <div class="user-box">
-            <textarea name="description" required placeholder="description :"></textarea>
-        </div>
-        <input type="hidden" name="id_personne" value="<?php
-        echo $_SESSION["me"]->id_personne;
-        ?>">
-        <button type="submit">Envoyer la demande</button>
-    </form>
-</div>
+    <div class="login-box">
+        <h2>Créer un nouveau sujet !</h2>
+        <form class="in_box" method="post">
+            <div class="user-box">
+                <input type="text" class="title" name="title" required value="<?php echo $question->titre ?>">
+                <label>Titre :</label>
+            </div>
+            <div class="user-box">
+                <select name="id_matiere" id="matiere-select" required>
+                    <option value="">Matière</option>
+                    <?php
+                    foreach (hget("http://localhost:4567/api/getMatiere") as $m) {
+
+                        echo $question->id_matiere == $m->id_matiere ? "<option value=" . $m->id_matiere . " selected='selected'>" . $m->intitule . "</option>" :
+                            "<option value=" . $m->id_matiere . ">" . $m->intitule . "</option>";}
+                    ?>
+                </select>
+            </div>
+            <div class="user-box">
+                <textarea name="description" required placeholder="description :" ><?php echo $question->description ?></textarea>
+            </div>
+            <input type="hidden" name="id_personne" value="<?php
+            echo $_SESSION["me"]->id_personne;
+            ?>">
+            <button type="submit">Envoyer la demande</button>
+        </form>
+    </div>
 <?php
 var_dump($question);
 if (!empty($_POST)) {
