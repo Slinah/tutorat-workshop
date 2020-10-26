25a64ed979536d084876f9b90c6d13d714ed930d
Slinah <cedric.menanteau@epsi.fr>
2020-10-26 09:06:36 +0100

    Maintenance

---
+++ views/maintenance.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Maintenance</title>
+  <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4.3.2/css/metro-all.min.css">
+  <link rel='stylesheet' href='/ressources/css/maintenance.css'>
+  <script src="https://cdn.metroui.org.ua/v4.3.2/js/metro.min.js"></script>
+</head>
+<body>
+        <div class="container pos-fixed pos-center">
+            <h1><span class="mif-warning ani-bounce fg-lightCrimson"></span> Ce site est en maintenance <span class="mif-warning ani-bounce fg-lightCrimson"></span></h1>
+                <div class="fg-black remark primary">
+                    Vous pouvez, pour vos demandes de cours, joindre Marion BARITEAU (EPSI B2), Priscillia DEZETTRE (WIS 2), Cédric MENANTEAU (EPSI I1).
+                </div>    
+        </div>
+    </body>
+</html>
