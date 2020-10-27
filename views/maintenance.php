a3531fd20cd7b5881a8882232292b917fe1c5af5
cedric <cedric.menanteau@nextdecision.com>
2020-10-27 15:19:57 +0100

    Super drole.

--- views/maintenance.php
+++ views/maintenance.php
@@ -8,7 +8,7 @@
 </head>
 <body>
         <div class="container pos-fixed pos-center">
-            <p><span class="mif-warning ani-bounce fg-lightCrimson"></span> Ce site est en maintenance <span class="mif-warning ani-bounce fg-lightCrimson"></span></p>
+            <p><span class="mif-warning ani-bounce fg-lightCrimson"></span> Ce site est en maintenance. <span class="mif-warning ani-bounce fg-lightCrimson"></span></p>
                 <div class="fg-black remark primary">
                     Vous pouvez, pour vos demandes de cours, joindre Marion BARITEAU (EPSI B2), Priscillia DEZETTRE (WIS 2), Cédric MENANTEAU (EPSI I1).
                 </div>    
